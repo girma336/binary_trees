5bc1f5d70c22ebf7cbd3ba3e565362be26274b49
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-24 08:09:14 +0300

    height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - count how many level do you have
+ * @tree: a pointer to the root node
+ * Return: size of parent
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t lef = 0, rig = 0;
+
+		lef = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		rig = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((lef > rig) ? lef : rig);
+	}
+
+	return (0);
+}
