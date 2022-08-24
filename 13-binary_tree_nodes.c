4b0d01c981f80435789d0b51e4b8bf12479959e3
Girma Tarekegn <girmatarekegn.gi@gmail.com>
2022-08-24 10:35:37 +0300

    nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - count how many level do you have
+ * binary_tree_nodes - count how many level do you have
  * @tree: a pointer to the root node
  * Return: size of parent
  */
 
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t size = 0;
 
@@ -14,8 +14,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		size += (tree->left || tree->right) ? 1 : 0;
 
-		size += binary_tree_leaves(tree->left);
-		size += binary_tree_leaves(tree->right);
+		size += binary_tree_nodes(tree->left);
+		size += binary_tree_nodes(tree->right);
 	}
 
 	return (size);
